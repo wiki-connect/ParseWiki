134b60de46701b08209bbf9857cd12f900ade469
ibrahem <ibrahem.al-radaei@outlook.com>
2025-02-20 01:52:33 +0300

    update

--- src/ParserCitations.php
+++ src/ParserCitations.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace WikiConnect\ParseWiki;
+
 use WikiConnect\ParseWiki\DataModel\Citation;
+
 class ParserCitations
 {
     private string $text;
     private array $citations;
-    public function __construct(string $text) {
+    public function __construct(string $text)
+    {
         $this->text = $text;
         $this->parse();
     }
-    private function find_sub_citations($string) {
+    private function find_sub_citations($string)
+    {
         preg_match_all("/<ref([^\/>]*?)>(.+?)<\/ref>/is", $string, $matches);
         return $matches;
     }
@@ -18,12 +23,13 @@ class ParserCitations
         $text_citations = $this->find_sub_citations($this->text);
         $this->citations = [];
         foreach ($text_citations[1] as $key => $text_citation) {
-            $_Citation = new Citation($text_citations[2][$key], $text_citation);
+            $_Citation = new Citation($text_citations[2][$key], $text_citation, $text_citations[0][$key]);
             $this->citations[] = $_Citation;
         }
     }
 
-    public function getCitations() : array {
+    public function getCitations(): array
+    {
         return $this->citations;
     }
 }
