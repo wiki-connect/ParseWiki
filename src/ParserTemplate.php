5970cf4300bf37711c421e90ecbbfe83a04b56e8
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-07-05 06:11:14 +0300

    Add __toString methods and improve template parsing
    Implemented __toString() methods for Attribute, ExternalLink, InternalLink, and Table classes to allow direct string conversion. Refactored ParserTemplate to extract pipe-clearing logic into a separate method. Enhanced ParserTemplates to use an iterative stack-based approach for nested template parsing with a configurable max depth, and improved template matching and retrieval.

--- src/ParserTemplate.php
+++ src/ParserTemplate.php
@@ -36,29 +36,42 @@ class ParserTemplate
      * This method does not return any value. It sets the internal state of the
      * object by populating the $name and $parameters properties.
      */
+    private function clear_pipes(string $DTemplate): string
+    {
+        $matches = [];
+        // preg_match_all("/\{\{(.*?)\}\}/s", $DTemplate, $matches);
+        preg_match_all("/\{\{((?:[^{}]++|(?R))*)\}\}/s", $DTemplate, $matches);
+
+        foreach ($matches[1] as $matche) {
+            $DTemplate = str_replace($matche, str_replace($this->pipe, $this->pipeR, $matche), $DTemplate);
+        }
+        $matches2 = [];
+
+        preg_match_all("/\[\[(.*?)\]\]/", $DTemplate, $matches2);
+        foreach ($matches2[1] as $matche) {
+            $DTemplate = str_replace($matche, str_replace($this->pipe, $this->pipeR, $matche), $DTemplate);
+        }
+
+        return $DTemplate;
+    }
     public function parse(): void
     {
         if (preg_match("/^\{\{(.*?)(\}\})$/s", $this->templateText, $matchesR)) {
-            $DTemplate = $matchesR[1];
-            $matches = [];
-            preg_match_all("/\{\{(.*?)\}\}/", $DTemplate, $matches);
-            foreach ($matches[1] as $matche) {
-                $DTemplate = str_replace($matche, str_replace($this->pipe, $this->pipeR, $matche), $DTemplate);
-            }
-            $matches = [];
-            preg_match_all("/\[\[(.*?)\]\]/", $DTemplate, $matches);
-            foreach ($matches[1] as $matche) {
-                $DTemplate = str_replace($matche, str_replace($this->pipe, $this->pipeR, $matche), $DTemplate);
-            }
+
+            $DTemplate = $this->clear_pipes($matchesR[1]);
 
             $params = explode("|", $DTemplate);
             $pipeR = $this->pipeR;
             $pipe = $this->pipe;
+
             $params = array_map(function ($string) use ($pipeR, $pipe) {
                 return str_replace($pipeR, $pipe, $string);
             }, $params);
+
             $data = [];
+
             $this->name = $params[0];
+
             for ($i = 1; $i < count($params); $i++) {
                 $param = $params[$i];
                 if (strpos($param, "=") !== false) {
@@ -84,4 +97,3 @@ class ParserTemplate
         return new Template($this->name, $this->parameters, $this->templateText);
     }
 }
-
